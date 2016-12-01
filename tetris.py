6480989c7c042dcd66d5f13236d6c8f02846dbfd
IanMcT <ian.mctavish@tldsb.on.ca>
2016-12-01 09:17:57 -0500

    variables
    Added section for global variables.

--- tetris.py
+++ tetris.py
@@ -3,3 +3,5 @@ import pygame
 """This is a Tetris style game that the ICS3U/4U 2016/2017 class in Huntsville High School is creating.
 Teacher: I. McTavish
 Date: Dec 1, 2016"""
+
+#Global Variables
